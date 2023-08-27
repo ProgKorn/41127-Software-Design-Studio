5bae54e876f69213add0c6c82d26cd5c0aba2260
Abrar Barbhuyia <abrarbarbhuyia@192-168-1-104.tpgi.com.au>
2023-08-27 21:06:08 +1000

    feat: added basic css, title and exam details title

---
+++ client/src/views/ExamStart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../css/Exam.css';
+
+function ExamStart() {
+  return (
+    <div className="main">
+      {/* Title */}
+      <div className="title">
+        <h1 >Exam Start</h1>
+      </div>
+      {/* Exam detail section */}
+      <div className="details">
+        <div>
+          <h2 >Exam Details</h2>
+        </div>
+        {/* Details table goes here */}
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExamStart;
