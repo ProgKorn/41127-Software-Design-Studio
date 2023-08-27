bc83f3a0fdc2ac44860ee756ac5ff5918a514762
jvqk <jack.w.marshall@student.uts.edu.au>
2023-08-27 15:22:21 +1000

    Removed Landing Page
    App.js acts as landing page, this was redundant

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const studentRoutes = require('./routes/student');
 const adminRoutes = require('./routes/admin');
 const authRoutes = require('./routes/auth');
 const examRoutes = require('./routes/exam');
-const landingRoutes = require('./routes/landing');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,7 +14,6 @@ app.use('/student', studentRoutes);
 app.use('/admin', adminRoutes);
 app.use('/auth', authRoutes);
 app.use('/exam', examRoutes);
-app.use('/landing', landingRoutes);
 
 // MongoDB Connection (do not commit plaintext credentials)
 
