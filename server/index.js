a6881f25948ab1d3f6f330ec58fff7a3c2445f5c
Z <13918317@student.uts.edu.au>
2023-08-31 19:56:31 +1000

    Initial Login Code (Function TBC)

--- server/index.js
+++ server/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const studentRoutes = require('./routes/student');
 const adminRoutes = require('./routes/admin');
-const authRoutes = require('./routes/auth');
+const loginRoutes = require('./routes/auth');
 const examRoutes = require('./routes/exam');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
+// Once post requests work consistently, change * to localhost:3000.
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use('/student', studentRoutes);
 app.use('/admin', adminRoutes);
-app.use('/auth', authRoutes);
+app.use('/login', loginRoutes);
 app.use('/exam', examRoutes);
 
 // MongoDB Connection (do not commit plaintext credentials)
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const router = express.Router();
+//const User = require('../models/User');
+//const bcrypt = require('bcrypt');
 
-// const User = require('../models/userModel');
+const User = [
+  { id: 1, username: 'admin', password: 'adminpassword', role: 'admin' },
+  { id: 2, username: 'student', password: 'studentpassword', role: 'user' },
+]; 
 
 router.get('/', (req, res) => {
-    res.json({ message: 'Login' });
+  res.json({ message: 'Login' });
+});
+
+router.post('/', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = User.find(user => user.username === username)
+
+    // const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // For now passwords are plaintext, the following code will be implemented once the DB is fully connected.
+    // const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (user.password !== password) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    if (user.role === 'admin') {
+      res.json({ role: 'admin' });
+    } else {
+      res.json({ role: 'user' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 module.exports = router;
