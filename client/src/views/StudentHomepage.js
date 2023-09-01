a79068421cc9a4af25f89b2a2015e487e0511397
Niyumi <niyumijaya@outlook.com>
2023-09-01 10:29:43 +1000

    Fixed up the formatting of front end elements

--- client/src/views/StudentHomepage.js
+++ client/src/views/StudentHomepage.js
@@ -20,11 +20,11 @@ function createData2(examName, examStart, details, seatNo, accessExam) {
 }
 
 const rows = [
-  createData('Full Name', 'John Doe'),
-  createData('Student ID', '123456789'),
-  createData('Email', 'test@gmail.com'),
-  createData('Institution', 'UTS'),
-  createData('Time Zone', 'Sydney')
+  createData('Full Name:', 'John Doe'),
+  createData('Student ID:', '123456789'),
+  createData('Email:', 'test@gmail.com'),
+  createData('Institution:', 'UTS'),
+  createData('Time Zone:', 'Sydney')
 ]
 
 const rows2 = [
@@ -36,47 +36,49 @@ function StudentHomepage() {
   return (
     <div className="main">
       <h1>Student Homepage</h1>
-      <div className="title">
-        <h2>Student Details</h2>
-        <div style={{ width: '40%', marginLeft: '0%' }}>
-        <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 100 }} aria-label="simple table">
+      <div class = 'parent' style = {{display: 'flex'}}>
+        <div class = 'child'>
+        <div className="title">
+          <h2>Student Details</h2>
+          <div style={{ width: '130%'}}>
+          <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 100 }} aria-label="simple table">
 
-          <TableBody>
-              {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.value}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>  
-    </div>
-    </div>
-      <div className="button-container" style={{ marginTop: '20px', textAlign:'left', marginLeft:'0px'}}>
+            <TableBody>
+                {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="left">{row.value}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>  
+      </div>
+      </div>
+      <div className="button-container" style={{ marginTop: '20px', textAlign:'left', marginLeft:'0px', width: '300px'}}>
         <Link to="/editDetails" className="button" style={{ fontSize: '12px', padding: '5px 10px'}}>
           Edit Details
         </Link>
       </div>
-
+      </div>
+    <div class = 'child' style = {{marginLeft: '15%'}} >
     <div className="title">
-        <h2>Upcoming Exam Scheduled</h2>
-        <div style={{ width: '50%', marginLeft: '0%' }}>
+        <h2>Upcoming Exams Scheduled</h2>
         <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell align="center">Exam Name</TableCell>
-              <TableCell align="center">Exam Start</TableCell>
-              <TableCell align="center">Details</TableCell>
-              <TableCell align="center">Seat No.</TableCell>
-              <TableCell align="center">Access Exam</TableCell>
+              <TableCell style={{ fontWeight: 'bold' }} align="center">Exam Name</TableCell>
+              <TableCell style={{ fontWeight: 'bold' }} align="center">Exam Start</TableCell>
+              <TableCell style={{ fontWeight: 'bold' }} align="center">Details</TableCell>
+              <TableCell style={{ fontWeight: 'bold' }} align="center">Seat No.</TableCell>
+              <TableCell style={{ fontWeight: 'bold' }} align="center">Access Exam</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -99,16 +101,29 @@ function StudentHomepage() {
         </TableContainer>
         </div>
     </div>
+    </div>
+    
     <div className="title">
         <h2>Test Your Equipment</h2>
-        <div className='testEquipment' style={{width:'100%'}}>
-        <text style={{width:'100%', marginTop:'10px'}}>It is highly advised that you check your computer system you are using beforehand to ensure a smooth online testing experience.
-        </text>
-      <div className="button-container" style={{width:'100%', height:'10vh', marginLeft:'0px', marginTop:'30px'}}>
-        <Link to="/testEquipment" className="button" style={{fontSize: '12px', padding: '5px 10px', marginLeft:'0px'}}>
+        <div className='testEquipment'>
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">It is highly advised that you check your computer system you are using beforehand to ensure a smooth online testing experience.</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+              <TableRow>
+                <TableCell align="center"> <div className="button-container" style={{width:'100%', height:'10vh', marginLeft:'0px', marginTop:'0px'}}>
+        <Link to="/testEquipment" className="studentButton" align = 'left'>
           Test Your Equipment
         </Link>
-      </div>
+      </div></TableCell>
+              </TableRow>
+          </TableBody>
+        </Table>
+        </TableContainer>
       </div>
     </div>
     </div>
