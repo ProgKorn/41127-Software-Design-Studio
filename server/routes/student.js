fe3defceb91601003b40cee9d872693ae06c46a6
tarakenishauy <92384503+tarakenishauy@users.noreply.github.com>
2023-09-04 15:32:55 +1000

    Some work done on Routing

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// const Student = require('../models/userModel');
+const Student = require('../models/userModel');
+const Exam = require('../models/examModel');
 
 router.get('/', (req, res) => {
     res.json({ message: 'Student' });
