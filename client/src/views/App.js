d5f34fe0b4cf5b64d992586f434c7e4211610a94
Georgette Bashour <13901057@student.uts.edu.au>
2023-09-05 23:55:09 +1000

    Added new route for facial landmark recognition

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -4,6 +4,7 @@ import Login from './Login';
 import Header from './Header';
 import HelpCentre from './HelpCentre';
 import ExamStart from './ExamStart';
+import FacialLandmarkRecognition from './FacialLandmarkRecognition';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/helpcentre" element={<HelpCentre />} />
           <Route path="/examstart" element={<ExamStart />} />
+          <Route path="/faciallandmarkrecognition" element={<FacialLandmarkRecognition />} />
         </Routes>
       </div>
     </Router>
