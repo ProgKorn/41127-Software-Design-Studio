f341373cf7f9e41ec33381a411da313baf7ff573
Georgette Bashour <13901057@student.uts.edu.au>
2023-09-14 17:34:16 +1000

    Modified function to detect cheating behaviours per frame

--- client/src/views/utilities.js
+++ client/src/views/utilities.js
@@ -1,21 +1,27 @@
-export const drawRect = (detections, ctx) =>{
-    // Loop through each prediction
-    detections.forEach(prediction => {
-  
-      // Extract boxes and classes
-      const [x, y, width, height] = prediction['bbox']; 
-      const text = prediction['class']; 
-  
-      // Set styling
-      const color = Math.floor(Math.random()*16777215).toString(16);
-      ctx.strokeStyle = '#' + color
-      ctx.font = '18px Arial';
-  
-      // Draw rectangles and text
-      ctx.beginPath();   
-      ctx.fillStyle = '#' + color
-      ctx.fillText(text, x, y);
-      ctx.rect(x, y, width, height); 
-      ctx.stroke();
-    });
-  }
+export const cheatingObject = (detections) => {
+  let personCounter = 0;
+  // Loop through each prediction
+  detections.forEach(prediction => {
+    
+    // Extract classes for each frame
+    const object = prediction['class']; 
+    const bannedObjects = ["cell phone", "laptop", "keyboard", "mouse"]; // Array of banned objects
+
+    // Cheating Conditions
+    switch (object) {
+      case "person":
+        personCounter++;
+        break;
+      case personCounter > 1:
+        console.log("Cheating Detected! More than one person in the room.");
+        // Raise Flagged Incident
+        break;
+      case bannedObjects.includes(object):
+        console.log("Cheating Detected! Banned object spotted in frame.")
+        // Raise Flagged Incident
+        break;
+      default:
+        console.log("No cheating detected.");
+    }
+  });
+}
