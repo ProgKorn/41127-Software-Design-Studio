7b0e261f69d7c0d1aacd891c7fea0307b3a45429
Georgette Bashour <13901057@student.uts.edu.au>
2023-09-14 23:22:26 +1000

    Detections for banned objects and person count functional for each prediction

--- client/src/views/utilities.js
+++ client/src/views/utilities.js
@@ -1,27 +1,57 @@
 export const cheatingObject = (detections) => {
-  let personCounter = 0;
+  var personCounter = 0; // Keep track of how many people are in the frame
   // Loop through each prediction
-  detections.forEach(prediction => {
+  detections.forEach(prediction => { // Looking at one prediction at a time
     
     // Extract classes for each frame
     const object = prediction['class']; 
     const bannedObjects = ["cell phone", "laptop", "keyboard", "mouse"]; // Array of banned objects
 
-    // Cheating Conditions
-    switch (object) {
-      case "person":
-        personCounter++;
-        break;
-      case personCounter > 1:
-        console.log("Cheating Detected! More than one person in the room.");
-        // Raise Flagged Incident
-        break;
-      case bannedObjects.includes(object):
-        console.log("Cheating Detected! Banned object spotted in frame.")
-        // Raise Flagged Incident
-        break;
-      default:
-        console.log("No cheating detected.");
+    if (object === "person") { // Is this prediction a person?
+      personCounter++; // Add to the person counter for the room
     }
+
+    var isCheating = objectConditions(object, bannedObjects, personCounter);
+    if (isCheating) {
+      // Raise Flagged Incident
+      console.log("I am raising a flag");
+    }
+  });
+}
+
+function objectConditions(object, bannedObjects, personCounter) { // Return true if cheating is detected
+  var cheating = false; // Initially, assume no cheating
+  // Cheating Conditions
+  if (personCounter > 1) { // Cheating Action -- more than one person in the room
+    cheating = true;
+    console.log("Cheating Detected! More than one person in the room.");
+  }
+  if (bannedObjects.includes(object)) { // Cheating Action -- using a banned object i.e cell phone
+    cheating = true;
+    console.log("Cheating Detected! Banned object spotted in frame.");
+  }
+
+  return cheating;
+}
+
+export const drawRect = (detections, ctx) => {
+  // Loop through each prediction
+  detections.forEach(prediction => {
+
+    // Extract boxes and classes
+    const [x, y, width, height] = prediction['bbox']; 
+    const text = prediction['class']; 
+
+    // Set styling
+    const color = Math.floor(Math.random()*16777215).toString(16);
+    ctx.strokeStyle = '#' + color
+    ctx.font = '18px Arial';
+
+    // Draw rectangles and text
+    ctx.beginPath();   
+    ctx.fillStyle = '#' + color
+    ctx.fillText(text, x, y);
+    ctx.rect(x, y, width, height); 
+    ctx.stroke();
   });
 }
