e1c41d1f8864f0c928e3daf540b3384b564394cc
Amy <amywlimpai@gmail.com>
2023-09-20 12:20:58 +1000

    Created tests for the database
    Created tests to assist with future use of the data base if needed

--- server/DatabaseAccess/call.js
+++ server/DatabaseAccess/call.js
@@ -1,20 +1,78 @@
-//run me with -> node call.js
+const { dbOp } = require('./databaseMaster.js');
 
-const { dbMaster } = require('./databaseMaster.js');
+async function testInsert() {
+  try {
+    const documents = [{ name: "TESTAlice", age: 25 }, { name: "TESTBob", age: 30 }];
+    await dbOp('insert', 'UserDetails', { docs: documents });
+    console.log('Insert operation succeeded');
+  } catch (error) {
+    console.log('Insert operation failed:', error);
+  }
+}
 
-//dbMaster('find', 'StudentDetails', { studentId: 12345678 });
+async function testDelete() {
+  try {
+    const query = { name: "TESTAlice" };
+    await dbOp('delete', 'UserDetails', { query });
+    console.log('Delete operation succeeded');
+  } catch (error) {
+    console.log('Delete operation failed:', error);
+  }
+}
+
 async function testFind() {
-    await db.dbOp('find', 'ExamDetails', { query: { 'examName': 'Maths' } });
+  try {
+    const query = { name: "TESTBob" };
+    const result = await dbOp('find', 'UserDetails', { query });
+    console.log('Find operation succeeded:', result);
+  } catch (error) {
+    console.log('Find operation failed:', error);
   }
+}
 
-//Tests
-  async function runTests() {
-    //await testInsert();
-    await testFind();
-    //await testUpdate();
-    //await testDelete();
+async function testExamFind() {
+  try {
+    const query = { examName: 'Maths' };
+    const result = await dbOp('find', 'ExamDetails', { query });
+    console.log('ExamFind operation succeeded:', result);
+  } catch (error) {
+    console.log('ExamFind operation failed:', error);
   }
-  
-  runTests().catch(console.error);
-  
-dbMaster('display');
+}
+
+async function testUpdateUser() {
+  try {
+    const email = "TESTBob@email.com";
+    const updateDoc = { age: 31 };
+    await dbOp('update-user', 'UserDetails', { email, updateDoc });
+    console.log('UpdateUser operation succeeded');
+  } catch (error) {
+    console.log('UpdateUser operation failed:', error);
+  }
+}
+
+async function testUpdateDoc() {
+  try {
+    const query = { name: "TESTBob" };
+    const docs = { $set: { age: 32 } };
+    await dbOp('update', 'UserDetails', { query, docs });
+    console.log('UpdateDoc operation succeeded');
+  } catch (error) {
+    console.log('UpdateDoc operation failed:', error);
+  }
+}
+
+async function runTests() {
+  try {
+    await testInsert();
+    await testDelete();
+    await testFind();
+    await testExamFind();
+    await testUpdateUser();
+    await testUpdateDoc();
+  } catch (error) {
+    console.error('An error occurred during testing:', error);
+  }
+}
+
+runTests().catch(console.error);
--- server/DatabaseAccess/databaseMaster.js
+++ server/DatabaseAccess/databaseMaster.js
@@ -1,29 +1,28 @@
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
 const uri = process.env.DATABASE_URI;
 const client = new MongoClient(uri);
 const bcrypt = require('bcrypt');
 
-// for inserting new data
 async function insertDoc(collType, docs) {
   const db = client.db("SoftwareDesignStudio");
   const coll = db.collection(collType);
   return await coll.insertMany(docs);
 }
 
-// for deleting students
 async function deleteDoc(collType, query) {
   const db = client.db("SoftwareDesignStudio");
   const coll = db.collection(collType);
-  return await coll.deleteMany({ query });
+  return await coll.deleteMany(query);
 }
 
 async function updateUser(email, updateDoc) {
   const db = client.db("SoftwareDesignStudio");
   const coll = db.collection("UserDetails");
-  // since we do not have a user registration page, this logic handles the password hashing.
   if (updateDoc.password) {
-    const saltRounds = 10; 
+    const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(updateDoc.password, saltRounds);
     updateDoc.password = hashedPassword;
   }
@@ -39,68 +38,41 @@ async function findDoc(collType, query) {
   return results;
 }
 
-async function updateDoc(collType, query, docs) {
+async function updateDocument(collType, query, docs) {  // Renamed to updateDocument
   const db = client.db("SoftwareDesignStudio");
   const coll = db.collection(collType);
   return await coll.updateMany(query, docs);
 }
 
 module.exports = {
-  // master function (entry represents the details)
   dbOp: async function (operationType, collType, entry) {
     await client.connect();
     let result;
     try {
-      const { query, docs, email, updateDoc } = entry; // Extract data from the entry object
+      const { query, docs, email, updateDoc } = entry;
       switch (operationType) {
-        
-        //Example usage:
-        // dbOp('insert', 'StudentDetails, [{ /* your document here */ }]
         case 'insert':
           result = await insertDoc(collType, docs);
-          //returns number of inserted documents
-          console.log("Inserted documents:", result.insertedIds);
-          //returns details of inserted documents
-          result.ops.forEach((doc, index) => {
-            console.log(`Document ${index + 1}:`, doc);
-          });
           break;
-
-        // Example usage:
-        // dbOp('delete', 'UserDetails, {email: 'steve@musk.com})
         case 'delete':
           result = await deleteDoc(collType, query);
-          console.log("Deleted count:", result.deletedCount);
           break;
-
-        // Example usage:
-        // // dbOp('find', 'ExamDetails', { query: { time: '10:00' } })
         case 'find':
-          result = await findDoc(collType, entry.query);
-          console.log("Found documents:", result);
+          result = await findDoc(collType, query);
           return result;
-
-        // Example usage:
-        // dbOp('update', { studentId : 12345678, updateDoc: { seatNumber : 200 }})
         case 'update-user':
-          result = await updateUser(entry.email, entry.updateDoc);
-          console.log("Updated count:", result.modifiedCount);
+          result = await updateUser(email, updateDoc);
           break;
-
-
-        // Example usage:
-        // dbOp('update', { studentId : 12345678, updateDoc: { seatNumber : 200 }})
         case 'update':
-          result = await updateDoc(collType, query, docs);
-          console.log("Update:", result.modifiedCount);
+          result = await updateDocument(collType, query, docs); // Renamed to updateDocument
           break;
         default:
           console.log("Invalid operation type.");
       }
-
+    } catch (error) {
+      console.error('An error occurred:', error);
     } finally {
       await client.close();
     }
   }
-
 };
