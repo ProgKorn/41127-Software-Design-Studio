aad5a9b56cdf40ff8b5e9cd22ea922489f9029fb
Georgette Bashour <13901057@student.uts.edu.au>
2023-09-21 14:35:35 +1000

    Updated flag schema

--- server/models/flagModel.js
+++ server/models/flagModel.js
@@ -4,11 +4,26 @@ const { Schema, model } = require('mongoose')
 var schema = Schema
 
 flagSchema = new schema({
+    flagId: {type: Number, required: true},
+    examId: {type: Number, required: true},
+    studentId: {type: Number, required: true},
+    status: {type: String, required: true},
+    description: {type: String, required: true},
+    sessionName: {type: String, required: true}
+})
+
+/* 
+
+Old Schema:
+
+flagSchema = new schema({
     examinee: {type: String, required: true},
     flag: {type: String, required: true},
     session: {type: String, required: true},
     session_no: {type: Number, required: true},
     status: {type: String, required: true}
 })
+
+*/
 
 module.exports = model("Flag", flagSchema)
