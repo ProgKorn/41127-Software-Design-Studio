a471a41edaf0fe4468eab2fa149f53e7665eadda
Georgette Bashour <13901057@student.uts.edu.au>
2023-10-01 17:11:28 +1100

    DB call CRUD operations progress

--- server/routes/flag.js
+++ server/routes/flag.js
@@ -12,10 +12,15 @@ router.get('/', (req, res) => {
 });
 */
 
-router.get('/getFlagDetails', async (req, res) => { // Get details for a specific flag
+router.get('/getFlagDetails', async (req, res) => { // Get details for a specific flag (or filter)
+    // Assume req.body will be in the form of { fieldName: %fieldName%, %fieldName%: %value% }
     try {
-        // Find singular flag from DB corresponding with a flagId
-        //await databaseMaster.dbOp('find', 'FlaggedIncidents', { query: { flagId: req.body.flagId } });
+        const filterName = req.body.fieldName; // "flagId"
+        const filterValue = req.body.filterName; // "abcdefg"
+
+        const query = {};
+        query[filterName] = filterValue;
+        await databaseMaster.dbOp('find', 'FlaggedIncidents', { query: query });
     } catch (error) {
         // Write up error message here
         console.error(error);
@@ -23,10 +28,9 @@ router.get('/getFlagDetails', async (req, res) => { // Get details for a specifi
     }
 });
 
-router.get('/getAllFlags', (req, res) => { // Find all flags in the collection
+router.get('/getAllFlags', async (req, res) => { // Find all flags in the collection
     try {
-        // fetch all documents in the FlaggedIncidents collection --> potentially add filter for student and/or class
-        //databaseMaster.dbOp('find', 'FlaggedIncidents');
+        await databaseMaster.dbOp('find', 'FlaggedIncidents');
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
@@ -55,18 +59,19 @@ router.post('/addFlag', async (req, res) => { // Add a new flag
     }
 });
 
-router.post('/deleteFlag', (req, res) => { // Delete a flag
+router.post('/deleteFlag', async (req, res) => { // Delete a flag associated with a specified flagId
     try {
-        // Delete Flag
+        await databaseMaster.dbOp('delete', 'FlaggedIncidents', req.body.flagId);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
-router.post('/updateFlag', (req, res) => { // Update status from Pending --> Resolved or Terminated
+router.post('/updateFlag', async (req, res) => { // Update status from Pending --> Resolved or Terminated
+    // Assuming req object will be in the form of { flagId: %flagId%, status: "Resolved" }
     try {
-        // Update Function
+        await databaseMaster.dbOp('update','FlaggedIncidents',{docs: []});
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
