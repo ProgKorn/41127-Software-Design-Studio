7246190ed9163ac7654b8b3185744928b5dc0397
Georgette Bashour <13901057@student.uts.edu.au>
2023-10-03 19:40:51 +1100

    Refactored API calls to use parameters, updateFlag is tested + added separate calls for each filtered GET request

--- server/routes/flag.js
+++ server/routes/flag.js
@@ -7,20 +7,28 @@ const { v4: uuidv4 } = require('uuid');
 
 /* 
 Example Implementation:
-router.get('/', (req, res) => {
-    res.json({ message: 'Flag' });
+
+1. Update Flag
+const updateObject = { 
+    flagId: "ed3a1dad-d9fb-4f19-8438-8308a2bb8e8f", 
+    status: "Terminated" 
+};
+
+axios.post(url + '/updateFlag', updateObject)
+.then((response) => {
+    console.log('Flag updated successfully: ', response.data);
+})
+.catch(error => {
+    console.error('Error adding flag: ', error);
 });
 */
 
-router.get('/getFlagDetails', async (req, res) => { // Get details for a specific flag (or filter)
-    // Assume req.body will be in the form of { fieldName: %fieldName%, %fieldName%: %value% }
+router.get('/getFlagDetails/:flagId', async (req, res) => { // Get details for a specific flag
     try {
-        const filterName = req.body.fieldName; // "flagId"
-        const filterValue = req.body.filterName; // "abcdefg"
-
-        const query = {};
-        query[filterName] = filterValue;
-        await databaseMaster.dbOp('find', 'FlaggedIncidents', { query: query });
+        const flagId = req.params.flagId;
+        await databaseMaster.dbOp('find', 'FlaggedIncidents', { query: { flagId: flagId } }).then(data => {
+            res.json(new Flag(data[0]));
+        });
     } catch (error) {
         // Write up error message here
         console.error(error);
@@ -28,9 +36,37 @@ router.get('/getFlagDetails', async (req, res) => { // Get details for a specifi
     }
 });
 
+router.get('/getStudentFlagDetails/:studentId', async (req, res) => { // Get all flagged incidents for a student
+    try {
+        const studentId = req.params.studentId;
+        await databaseMaster.dbOp('find', 'FlaggedIncidents', { query: { studentId: studentId } }).then((data) => {
+            const flagObjects = data.map((flagData) => new Flag(flagData));
+            res.json(flagObjects);
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.get('/getExamFlagDetails/:examId', async (req, res) => { // Get all flagged incidents for an exam
+    try {
+        const examId = req.params.examId;
+        await databaseMaster.dbOp('find', 'FlaggedIncidents', { query: { examId: examId } }).then(data => {
+            const flagObjects = data.map((flagData) => new Flag(flagData));
+            res.json(flagObjects);
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 router.get('/getAllFlags', async (req, res) => { // Find all flags in the collection
     try {
-        await databaseMaster.dbOp('find', 'FlaggedIncidents');
+        await databaseMaster.dbOp('find', 'FlaggedIncidents', { query: {} }).then(data => {
+            res.json(data);
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
@@ -43,25 +79,26 @@ router.post('/addFlag', async (req, res) => { // Add a new flag
         const newFlagId = uuidv4();
         console.log(newFlagId);
 
-        const newFlag = new Flag ({ // Hardcoded student and exam for now
+        const newFlag = new Flag ({
             flagId: newFlagId.toString(),
-            examId: 3, // Maths
-            studentId: 12345678, // Jane Doe
+            examId: req.body.params.examId, 
+            studentId: req.body.params.studentId,
             status: "Pending",
             description: req.body.flagType,
-            sessionName: "Maths Exam Friday 4pm"
+            sessionName: req.body.params.sessionName,
         });
-        await databaseMaster.dbOp('insert', 'FlaggedIncidents', {docs: [newFlag]});
+        const flag = await databaseMaster.dbOp('insert', 'FlaggedIncidents', { docs: [newFlag] });
         console.log("I have raised this flag " + newFlag);
+        res.json(flag);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
-router.post('/deleteFlag', async (req, res) => { // Delete a flag associated with a specified flagId
+router.post('/deleteFlag/:flagId', async (req, res) => { // Delete a flag associated with a specified flagId
     try {
-        await databaseMaster.dbOp('delete', 'FlaggedIncidents', req.body.flagId);
+        await databaseMaster.dbOp('delete', 'FlaggedIncidents', { query: { flagId: req.params.flagId } } );
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
@@ -69,9 +106,16 @@ router.post('/deleteFlag', async (req, res) => { // Delete a flag associated wit
 });
 
 router.post('/updateFlag', async (req, res) => { // Update status from Pending --> Resolved or Terminated
-    // Assuming req object will be in the form of { flagId: %flagId%, status: "Resolved" }
+    // Assuming req object will be in the form of { flagId: "test", status: "Resolved" }
     try {
-        await databaseMaster.dbOp('update','FlaggedIncidents',{docs: []});
+        // Extract flagId and new status from the request body
+        const { flagId, status } = req.body;
+        const query = { flagId: flagId };
+        const docs = { $set: { status: status } };
+
+        // Update the flag in the database by specifying the query criteria and update data
+        await databaseMaster.dbOp('update', 'FlaggedIncidents', { query, docs });
+        res.json({ message: 'Flag updated successfully' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
