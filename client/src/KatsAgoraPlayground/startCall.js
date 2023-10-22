f98a4bf05f974277cd963d5992f9087ba9d8e1a2
katrinageorge <katrina.g027@gmail.com>
2023-10-22 21:13:24 +1100

    basic fixes

--- client/src/KatsAgoraPlayground/startCall.js
+++ client/src/KatsAgoraPlayground/startCall.js
@@ -1,21 +1,29 @@
-import './startCall.css'
+import "./startCall.css";
 import AgoraRTC from "agora-rtc-sdk-ng";
 import { useState, useEffect } from "react";
+import { Button, Grid } from "@mui/material";
 
 const secrets = {
   appId: "e5709f8be2604869864acfa71a1f8b42",
   channelName: "main",
-  token: "007eJxTYJCrqJmy7ruBOOeRitQupsRft9Uf3nqokNxX4bRw46/TwtMUGFJNzQ0s0yySUo3MDEwszCwtzEwSk9MSzQ0TDYGiJka+i01SGwIZGarFNjMwQiGIz8KQm5iZx8AAAFqBH0o=",
+  token:
+    "007eJxTYJCrqJmy7ruBOOeRitQupsRft9Uf3nqokNxX4bRw46/TwtMUGFJNzQ0s0yySUo3MDEwszCwtzEwSk9MSzQ0TDYGiJka+i01SGwIZGarFNjMwQiGIz8KQm5iZx8AAAFqBH0o=",
 };
 
 const rtc = {
   client: null,
   localVideoTrack: null,
   localAudioTrack: null,
+  localAudioAndVideoTrack: null,
 };
 
 async function join() {
-  rtc.client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8", App: secrets.appId, token: secrets.token });
+  rtc.client = AgoraRTC.createClient({
+    mode: "rtc",
+    codec: "vp8",
+    App: secrets.appId,
+    token: secrets.token,
+  });
   await rtc.client.join(secrets.appId, "main", secrets.token);
 }
 
@@ -46,9 +54,9 @@ async function startCall() {
 const App = () => {
   const [videoOn, setVideoOn] = useState(false);
   const [joined, setJoined] = useState(false);
-  
+
   console.log(process.env);
-  
+
   const handleCamera = () => {
     if (videoOn) {
       stopVideo();
@@ -59,35 +67,27 @@ const App = () => {
     }
   };
 
-  // useEffect(() => {
-  //   startCall();
-  //   setJoined(true);
-  // }, []);
+  const handleStart = () => {
+    startCall();
+    startVideo();
+    setVideoOn(true);
+  };
 
   return (
     <div>
       <h1>Hello</h1>
       {!joined ? (
-        <button
+        <Button
           onClick={() => {
-            startCall();
+            handleStart();
             setJoined(true);
           }}
         >
-          Join Call
-        </button>
+          I consent to being recorded
+        </Button>
       ) : (
-        <button
-          onClick={() => {
-            leaveCall();
-            setJoined(false);
-            setVideoOn(false);
-          }}
-        >
-          End Call
-        </button>
+        <Button disabled>I consent to being recorded</Button>
       )}
-      {joined && <button onClick={handleCamera}>Turn {videoOn ? "off" : "on"} Video</button>}
       <div id="videoContainer"></div>
     </div>
   );
