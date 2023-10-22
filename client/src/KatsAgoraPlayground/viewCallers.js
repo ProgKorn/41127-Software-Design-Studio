d0d3f82e6581ec6b22df3b4349213dfa5d9ad476
katrinageorge <katrina.g027@gmail.com>
2023-10-22 21:22:46 +1100

    sample admin (WIP)

---
+++ client/src/KatsAgoraPlayground/viewCallers.js
@@ -0,0 +1,89 @@
+import AgoraRTC from "agora-rtc-sdk-ng";
+import { useState } from "react";
+import './startCall.css'
+
+const secrets = {
+  appId: "e5709f8be2604869864acfa71a1f8b42",
+  channelName: "main",
+  token: "007eJxTYNB0nuUsMOl904xD4uy3jqY/vvB/d+j/vkixzz1Bcw56CdQqMKSamhtYplkkpRqZGZhYmFlamJkkJqclmhsmGgJFTYz01pukNgQyMqy6/Z2ZkQECQXwWhtzEzDwGBgCW6SDD",
+};
+
+const rtc = {
+  client: null,
+  localVideoTrack: null,
+  localAudioTrack: null,
+};
+
+async function join() {
+  rtc.client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8", App: secrets.appId, token: secrets.token });
+  await rtc.client.join(secrets.appId, "main", secrets.token);
+}
+
+async function leaveCall() {
+  await stopVideo();
+  rtc.client.leave();
+}
+
+const startVideo = async () => {
+  rtc.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
+  console.log("Connection State: ", rtc.client.connectionState);
+  if (rtc.client.connectionState === "CONNECTED") {
+    rtc.client.publish(rtc.localVideoTrack);
+    rtc.localVideoTrack.play("videoContainer", { fit: "fill" });
+  }
+};
+
+const stopVideo = async () => {
+  rtc.localVideoTrack.close();
+  rtc.localVideoTrack.stop();
+  rtc.client.unpublish(rtc.localVideoTrack);
+};
+
+async function startCall() {
+  await join();
+}
+
+const App = () => {
+  const [videoOn, setVideoOn] = useState(false);
+  const [joined, setJoined] = useState(false);
+  console.log(process.env);
+  const handleCamera = () => {
+    if (videoOn) {
+      stopVideo();
+      setVideoOn(false);
+    } else {
+      startVideo();
+      setVideoOn(true);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Hello</h1>
+      {!joined ? (
+        <button
+          onClick={() => {
+            startCall();
+            setJoined(true);
+          }}
+        >
+          Join Call
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            leaveCall();
+            setJoined(false);
+            setVideoOn(false);
+          }}
+        >
+          End Call
+        </button>
+      )}
+      {joined && <button onClick={handleCamera}>Turn {videoOn ? "off" : "on"} Video</button>}
+      <div id="videoContainer"></div>
+    </div>
+  );
+};
+
+export default App;
