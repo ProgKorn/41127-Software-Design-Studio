8931c7adee8ca8b6c6a53a9f960470f2b47e3332
Georgette Bashour <13901057@student.uts.edu.au>
2023-10-26 16:14:19 +1100

    Add flag event and notification functionality, text + icon

--- client/src/components/FlagNotification.js
+++ client/src/components/FlagNotification.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
+import "../css/FlagNotification.css";
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 
 const FlagNotification = () => {
   const [flagAdded, setFlagAdded] = useState(false);
@@ -8,21 +10,34 @@ const FlagNotification = () => {
     const socket = io('http://localhost:4001'); // Change the URL to match your Socket.io server's URL
     socket.on('connect', () => {
       console.log(`You connected with id: ${socket.id}`);
-      // move emit here to make sure you're always connected before sending events if needed
+      // Emit here to make sure you're always connected before sending events if needed
     })
-    socket.emit('custom-event', 10, 'Hi', {a: 'a'});
+    socket.on('add-flag', () => { // When a flag is added
+      setFlagAdded(true);
+    })
   }, []);
+
+  const closeNotification = () => { // Close Pop Up
+    setFlagAdded(false);
+  };
 
   return (
     <div>
       {flagAdded && (
         <div className="popup-notification">
-          New Flag Added!
+          <div className="popup-content">
+            <div className="warning-icon">
+              <WarningAmberIcon/>
+            </div>
+            <div className="notification-text">
+              <p>You have been flagged for academic misconduct. You have one remaining flag before your exam session is ended.</p>
+            </div>
+            <button onClick={closeNotification} className="ok-button">OK</button>
+          </div>
         </div>
       )}
-      {/* Rest of your application */}
     </div>
-  );
+  );  
 };
 
 export default FlagNotification;
