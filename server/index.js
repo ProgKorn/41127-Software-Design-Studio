8230dc81c2e9282275013dca7c557e7af560b0d1
Georgette Bashour <13901057@student.uts.edu.au>
2023-10-26 14:31:44 +1100

    Added socket-io server connection + moved /addFlag endpoint

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ const adminRoutes = require('./routes/admin');
 const authRoutes = require('./routes/auth');
 const examRoutes = require('./routes/exam');
 const flagRoutes = require('./routes/flag');
+const { v4: uuidv4 } = require('uuid'); // Import uuidv4 from the uuid library
+const Flag = require('/Users/georgettebashour/Projects/41127-Software-Design-Studio/server/models/flagModel.js');
 
 const app = express();
 const PORT = 4000;
@@ -23,6 +25,19 @@ app.use('/class', classRoutes);
 app.use('/admin', adminRoutes);
 app.use('/auth', authRoutes);
 app.use('/exam', examRoutes);
+
+const io = require('socket.io')(4001, {
+  cors: {
+    origin: ['http://localhost:3000'] // client URL
+  }
+});
+
+io.on('connection', socket => { // function that runs everytime a client connects to our server, give a socket instance for each one
+  console.log(socket.id); // each person who connects to our server is assigned an ID
+  socket.on('custom-event', (number, string, obj) => {
+    console.log(number, string, obj);
+  })
+});
 
 app.post('/login', async(req, res) => {
   const { username, password, keepSignedIn } = req.body;
@@ -53,4 +68,28 @@ app.post('/login', async(req, res) => {
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.post('/addFlag', async (req, res) => { // Add a new flag
+  try {
+      // Create a new flag instance
+      const newFlagId = uuidv4();
+      console.log(newFlagId);
+
+      const newFlag = new Flag ({
+          flagId: newFlagId.toString(),
+          examId: req.body.examId, 
+          studentId: req.body.studentId,
+          status: "Pending",
+          description: req.body.flagType,
+          sessionName: req.body.sessionName,
+      });
+      const flag = await dbOp('insert', 'FlaggedIncidents', { docs: [newFlag] });
+      //io.emit('flag-added',{ message: 'New flag added!' });
+      console.log("I have raised this flag " + newFlag);
+      res.json(flag);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
