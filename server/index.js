103493eb5db527e2b15a462c4628967db8423d62
Georgette Bashour <13901057@student.uts.edu.au>
2023-10-29 00:18:21 +1100

    Removed old commented code

--- server/index.js
+++ server/index.js
@@ -26,19 +26,6 @@ app.use('/admin', adminRoutes);
 app.use('/auth', authRoutes);
 app.use('/exam', examRoutes);
 
-// const io = require('socket.io')(4001, {
-//   cors: {
-//     origin: ['http://localhost:3000'] // client URL
-//   }
-// });
-
-// io.on('connection', socket => { // function that runs everytime a client connects to our server, give a socket instance for each one
-//   console.log(socket.id); // each person who connects to our server is assigned an ID
-//   socket.on('custom-event', (number, string, obj) => {
-//     console.log(number, string, obj);
-//   })
-// });
-
 app.post('/login', async(req, res) => {
   const { username, password, keepSignedIn } = req.body;
 
@@ -69,27 +56,3 @@ app.post('/login', async(req, res) => {
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// app.post('/addFlag', async (req, res) => { // Add a new flag
-//   try {
-//       // Create a new flag instance
-//       const newFlagId = uuidv4();
-//       console.log(newFlagId);
-
-//       const newFlag = new Flag ({
-//           flagId: newFlagId.toString(),
-//           examId: req.body.examId, 
-//           studentId: req.body.studentId,
-//           status: "Pending",
-//           description: req.body.flagType,
-//           sessionName: req.body.sessionName,
-//       });
-//       const flag = await dbOp('insert', 'FlaggedIncidents', { docs: [newFlag] });
-//       io.emit('add-flag');
-//       console.log("I have raised this flag " + newFlag);
-//       res.json(flag);
-//   } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
