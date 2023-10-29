7db7cb9dd917de0038ca2fb1c90bdf19165cec11
Abrar Barbhuyia <abrarbarbhuyia@192-168-1-107.tpgi.com.au>
2023-10-29 19:20:27 +1100

    feat: adding server config + cors policy for live deployment

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const flagRoutes = require('./routes/flag');
 const app = express();
 const PORT = 4000;
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/flag', flagRoutes);
 app.use('/student', studentRoutes);
@@ -26,6 +26,11 @@ app.use('/auth', authRoutes);
 app.use('/exam', examRoutes);
 app.use('/examStudent', examStudentRoutes);
 
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://sentinel-frontend.vercel.app'],
+};
+
+// Configure CORS to allow requests from http://localhost:3000 and https://sentinel-frotnend.vercel.app
 app.post('/login', async(req, res) => {
   const { username, password, keepSignedIn } = req.body;
 
