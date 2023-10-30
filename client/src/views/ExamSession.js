7939679f287975dd522646fcc8990af016886273
Georgette Bashour <13901057@student.uts.edu.au>
2023-10-30 19:27:56 +1100

    Removed duplicate ObjectRecognition component from Exam Session view

--- client/src/views/ExamSession.js
+++ client/src/views/ExamSession.js
@@ -175,7 +175,6 @@ function ExamSession() {
       {cameraPermission ? (
       <>
         <ObjectRecognition examInProgress={examInProgress} />
-        <ObjectRecognition />
         <FlagNotification />
       </>
       ) : (
